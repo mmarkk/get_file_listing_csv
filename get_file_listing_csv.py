8ed4cf131ce8974960031258509cc5f5f02e8e2f
mmarkk <mmarkk@xtra.co.nz>
2023-02-17 09:44:15 +1300

    Initial Commit

--- get_file_listing_csv.py
+++ get_file_listing_csv.py
@@ -2,6 +2,9 @@
 Generates a CSV file listing all files in the current directory and all subdirectories.
 Directories are listed before the files in that directory.
 Filenames are stripped of their file suffix.
+
+The CSV file is saved to the current directory with the name "DirectoryListing.csv".
+This file must be run in the root directory of the files to be listed.
 '''
 
 import os
