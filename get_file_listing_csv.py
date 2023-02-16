335a60c0796849b5d8a59e3c08773e65d178cb73
mmarkk <mmarkk@xtra.co.nz>
2023-02-17 09:43:33 +1300

    Initial commit

---
+++ get_file_listing_csv.py
@@ -0,0 +1,39 @@
+'''
+Generates a CSV file listing all files in the current directory and all subdirectories.
+Directories are listed before the files in that directory.
+Filenames are stripped of their file suffix.
+'''
+
+import os
+import csv
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Create a list to store all the file names
+file_list = []
+
+# Walk through all directories and files in the current directory
+for dirpath, dirnames, filenames in os.walk(current_dir):
+    # Add each file name to the file list
+    for filename in filenames:
+        # Get the file name without the file suffix
+        file_name = os.path.splitext(filename)[0]
+        file_list.append(file_name)
+
+# Write the file list to a CSV file
+with open('DirectoryListing.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    # Write the header row
+    writer.writerow(['Directory Name', 'File Name'])
+    # Loop through each directory and its files and write them to the CSV file
+    for dirpath, dirnames, filenames in os.walk(current_dir):
+        # Write the directory name before the files in this directory
+        writer.writerow([dirpath, ''])
+        for filename in filenames:
+            # Get the file name without the file suffix
+            file_name = os.path.splitext(filename)[0]
+            # Write the file name to the CSV file
+            writer.writerow(['', file_name])
+
+print(f"Directory listing has been saved to {os.path.join(current_dir, 'DirectoryListing.csv')}")
